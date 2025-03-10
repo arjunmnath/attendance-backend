69bbb69a23489c5d15e69e6d78b59eb348838375
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-03-10 12:43:51 +0530

    Note: modified project structure to match vercel requirements

--- main.go
+++ main.go
@@ -1,49 +1,15 @@
 package main
 
 import (
-	"attendance-backend/controllers"
-	"attendance-backend/db"
-	"attendance-backend/models"
+    "attendance-backend/handler"
 	"fmt"
 	"log"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-
-	godotenv.Load()
-	db.ConnectDatabase()
-
-	// For production only, drops the preexisting tables and creates new ones
-	db.DB.Migrator().DropTable(&models.Device{}, &models.CurrentEvents{}, &models.Attendance{})
-	err := db.DB.AutoMigrate(&models.Device{}, &models.CurrentEvents{}, &models.Attendance{})
-
-	if err != nil {
-		log.Fatal("Failed to migrate the database", err)
-	}
-	Seeder()
-	log.Println("Database migrated successfully")
-
-	r := gin.Default()
-	// routes.RegisterDeviceRoutes(r)
-
-	// Routes
-
-	r.POST("/create-event", controllers.InitaiteEvent)
-	r.GET("/events", controllers.GetEvents)
-	r.GET("/event/:id", controllers.GetDevicesInEvent)
-	r.DELETE("/delete-event/:id", controllers.DeleteEvent)
-	r.GET("/event/graph/:id", controllers.GetEventGraph)
-
-	r.POST("/device/participate", controllers.Participate)
-	r.POST("/device/proximity-update", controllers.ProximityUpdate)
-	r.GET("/connectSSE/:event_id", controllers.ConnectSSE)
-
-	for _, route := range r.Routes() {
+	for _, route := range handler.Engine.Routes() {
 		fmt.Println(route.Method, route.Path)
 	}
 	log.Println("Server started at :8080")
-	r.Run(":8080")
+	handler.Engine.Run(":8080")
 }
